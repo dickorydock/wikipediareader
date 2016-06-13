15ef51aeb2a2ddc8c6b82c80b5d261dd45079106
Dickory Dock <nickcommins@gmail.com>
2016-06-13 17:11:38 -0400

    adding ajax 7

--- js/wikipediareader.js
+++ js/wikipediareader.js
@@ -13,18 +13,21 @@ var searchFunction = function(){
   // $("#testBox").html(htmlstring);
   // $("searchtext").value="";
 
- 
-    $.ajax({
+  $.ajax({
         type: "GET",
-        url: "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=Hendrix",
-        // contentType: "application/json; charset=utf-8",
+        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+        contentType: "application/json; charset=utf-8",
         async: false,
         dataType: "json",
         success: function (data, textStatus, jqXHR) {
             console.log(data);
-        },
+
+            $("#testBox").html(data);          },
         error: function (errorMessage) {
         }
+    });
+
+
     });
 
 
