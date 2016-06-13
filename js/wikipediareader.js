625caf5b63c4f4e6e1971dd537565e2504bcfef1
Dickory Dock <nickcommins@gmail.com>
2016-06-13 17:02:09 -0400

    adding ajax 2

--- js/wikipediareader.js
+++ js/wikipediareader.js
@@ -6,55 +6,65 @@ var searchFunction = function(){
   htmlstring="";
   if (exstring.length>0){
   for (i=0; i<arrstring.length; i++){
-    htmlstring+="<div class='newentry'><br><div class='articletitle'>"+"Random Title"+"</div><div class='articletext'>"+arrstring[i]+"</div></div>";
-    
+    htmlstring+="<div class='newentry'><br><div class='articletitle'>"+"Random Title"+"</div><div class='articletext'>"+arrstring[i]+"</div></div>";    
   }
   }
   wikijson = "end of it";
   $("#testBox").html(htmlstring);
   $("searchtext").value="";
   
-var openWeatherMapKey = '3da93211bae1ede6af6a70a1ab2e1fee';
-  
-  var requestString = "https://api.openweathermap.org/data/2.5/weather?lat=43.4&lon=74.5"+ "&APPID="+openWeatherMapKey;
-  
+  $(document).ready(function(){
+ 
+    $.ajax({
+        type: "GET",
+        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+            console.log(data);
+        },
+        error: function (errorMessage) {
+        }
+    });
+});
+
   // requestString="http://cors.io/?u=http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein";
-  requestString ="http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert";
-  console.log("are you there god");
-  // $httpn(function(response){ console.log(response); })
-  request = new XMLHttpRequest();
-    request.onreadystatechange=function(){
-      console.log("its me");
-      request.setRequestHeader("Origin", "http://dickorydock.github.io");
-      console.log("first" +request.readyState + " second "+request.status);
-      if (request.readyState==4&&request.status==200){
-        //parse the JSON
-        console.log(request);
-        console.log(request.responseText);
-        // parsedWiki=JSON.parse(request.responseText);
-        $("#printjson").html(request.responseText);
-      }
+  // requestString ="http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert";
+  // // $httpn(function(response){ console.log(response); })
+  // request = new XMLHttpRequest();
+  //   request.onreadystatechange=function(){
 
-    }
-    request.open("get", requestString, true);
-    request.send();
+  //     // request.setRequestHeader("Origin", "http://dickorydock.github.io");
+  //     console.log("first" +request.readyState + " second "+request.status);
+  //     if (request.readyState==4&&request.status==200){
+  //       //parse the JSON
+  //       console.log(request);
+  //       console.log(request.responseText);
+  //       // parsedWiki=JSON.parse(request.responseText);
+  //       $("#printjson").html(request.responseText);
+  //     }
 
-$.ajax( {
-    url: 'https://en.wikipedia.org/w/api.php',
-    data: {
-        action: 'query',
-        meta: 'userinfo',
-        format: 'json',
-        origin: 'https://www.mediawiki.org'
-    },
-    xhrFields: {
-        withCredentials: true
-    },
-    dataType: 'json'
-} ).done( function ( data ) {
-  console.log("whts up");
-  console.log(data);
-} );
+  //   }
+  //   request.open("get", requestString, true);
+  //   request.send();
+
+// $.ajax( {
+//     url: 'https://en.wikipedia.org/w/api.php',
+//     data: {
+//         action: 'query',
+//         meta: 'userinfo',
+//         format: 'json',
+//         origin: 'https://www.mediawiki.org'
+//     },
+//     xhrFields: {
+//         withCredentials: true
+//     },
+//     dataType: 'json'
+// } ).done( function ( data ) {
+//   console.log("whts up");
+//   console.log(data);
+// } );
 
 }
 
@@ -69,3 +79,8 @@ $("#submitForm").submit(function(){
 
   // console.log(arrstring);
   // htmlstring = "<br>want<br>you<br>now<br>"
+
+// var openWeatherMapKey = '3da93211bae1ede6af6a70a1ab2e1fee';
+  
+//   var requestString = "https://api.openweathermap.org/data/2.5/weather?lat=43.4&lon=74.5"+ "&APPID="+openWeatherMapKey;
+//   
