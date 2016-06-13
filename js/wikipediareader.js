2dc9cd414cff455d2d0c68d0b5f222c1ad9f217b
Dickory Dock <nickcommins@gmail.com>
2016-06-13 16:25:51 -0400

    fixed header 3

--- js/wikipediareader.js
+++ js/wikipediareader.js
@@ -19,7 +19,7 @@ var openWeatherMapKey = '3da93211bae1ede6af6a70a1ab2e1fee';
   var requestString = "https://api.openweathermap.org/data/2.5/weather?lat=43.4&lon=74.5"+ "&APPID="+openWeatherMapKey;
   
   // requestString="http://cors.io/?u=http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein";
-  requestString ="Access-Control-Allow-Origin: http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert";
+  requestString ="http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert";
   console.log("are you there god");
   // $httpn(function(response){ console.log(response); })
   request = new XMLHttpRequest();
