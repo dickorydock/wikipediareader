3ca669177fb826a8e2452d544b2f856e65bf9d7d
Dickory Dock <nickcommins@gmail.com>
2016-06-14 00:17:40 -0400

    new test page 2

--- js/wikipediareader2.js
+++ js/wikipediareader2.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
-
   console.log("this is it.");
+  $.ajax({url: "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein", success: function(result){
+    console.log('test1');
+        console.log(result);
+    }, error: function(result){console.log("oh no2");} 
+  });
+  console.log("but 3");
 });
